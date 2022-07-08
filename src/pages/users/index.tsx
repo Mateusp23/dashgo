a3054a3f50141988b194f8a0d88015f63e261eb8
Mateus Paulart <mateusp23@rede.ulbra.br>
2022-07-08 17:43:27 -0300

    listando users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -82,31 +82,35 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="green"/>
-                    </Td>
-                    <Td px={["4", "4", "6"]}>
-                      <Box>
-                        <Text fontWeight="bold">Mateus Paulart</Text>
-                        <Text fontSize="sm" color="gray.300">mateuspaulart@gmail.com</Text>
-                      </Box>
-                    </Td> 
-                    { isWideVersion && <Td>04 de Abril 2022</Td> }             
-                    { isWideVersion && 
-                      <Td>
-                        <Button 
-                          as="a" 
-                          size="sm" 
-                          fontSize="sm"
-                          colorScheme="purple"
-                          leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                        >
-                          Editar        
-                        </Button>
-                      </Td>       
-                    }        
-                  </Tr>
+                  {data.users.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="green"/>
+                        </Td>
+                        <Td px={["4", "4", "6"]}>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                          </Box>
+                        </Td> 
+                        { isWideVersion && <Td>{user.createdAt}</Td> }             
+                        { isWideVersion && 
+                          <Td>
+                            <Button 
+                              as="a" 
+                              size="sm" 
+                              fontSize="sm"
+                              colorScheme="purple"
+                              leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                            >
+                              Editar        
+                            </Button>
+                          </Td>       
+                        }        
+                      </Tr>
+                    )
+                  })}
                 </Tbody>
               </Table>
 
