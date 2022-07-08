08abee090907a5cc30b6dac144b481895d731800
Mateus Paulart <mateusp23@rede.ulbra.br>
2022-07-08 16:43:15 -0300

    criando users fakes com faker e Factories e Seeds

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,6 +14,7 @@ import {
   Text,
   useBreakpointValue
 } from '@chakra-ui/react';
+import { useEffect } from 'react';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
@@ -25,6 +26,12 @@ export default function UserList() {
     base: false,
     lg: true,
   });
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
 
   return (
     <Box>
